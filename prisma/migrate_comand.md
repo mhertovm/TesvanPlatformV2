23958ff8bfda19533974155ef1ce5b7c2efecf27
mhertovm <mhertovmasyan904@gmail.com>
2025-10-24 12:56:46 +0400

    added prisma orm

---
+++ prisma/migrate_comand.md
@@ -0,0 +1,17 @@
+-- create new migrate --
+npx prisma migrate dev --name migration_name
+
+-- migrate --
+npx prisma migrate deploy
+
+-- delete db data and migrate --
+npx prisma migrate reset
+
+-- seed --
+npm run seed
+
+-- view db --
+npx prisma studio
+
+
+
---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,10 @@
+import { Global, Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Global() // makes it accessible everywhere
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+export class PrismaModule {}
+
